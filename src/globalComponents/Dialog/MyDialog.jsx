3af8704c59e27fdf679d090319440f39b5958be1
levanthien1812 <20521947@gm.uit.edu.vn>
2023-06-01 14:54:23 +0700

    add admin mange donors & events pages

---
+++ src/globalComponents/Dialog/MyDialog.jsx
@@ -0,0 +1,35 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import React from "react";
+
+function MyDialog({ title = "Thông báo", message, handleClose, handleAccept }) {
+  return (
+    <Dialog
+      open={true}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {message}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Hủy</Button>
+        <Button onClick={handleClose} autoFocus>
+          OK
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default MyDialog;
--- src/globalComponents/Header/HeaderNotification.jsx
+++ src/globalComponents/Header/HeaderNotification.jsx
@@ -4,22 +4,22 @@ import BellSVG from '../../assets/images/global/bell.svg'
 export default function HeaderNotification() {
   return (
     <div >
-        <div class="dropdown">
+        <div className="dropdown">
             <a className="me-3 hidden-arrow" href="#" id="navbarDropdownMenuLink"
             role="button" data-mdb-toggle="dropdown" aria-expanded="false">
                 <img src={BellSVG} alt="bell"
                     style={{lineHeight: '42px', height: '42px', padding: '12px'}}/>
                 {/* <span class="badge rounded-pill badge-notification bg-danger">1</span> */}
             </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                 <li>
-                    <p class="dropdown-item">Some news</p>
+                    <p className="dropdown-item">Some news</p>
                 </li>
                 <li>
-                    <p class="dropdown-item">Some news</p>
+                    <p className="dropdown-item">Some news</p>
                 </li>
                 <li>
-                    <p class="dropdown-item">Some news</p>
+                    <p className="dropdown-item">Some news</p>
                 </li>
             </ul>
         </div>
